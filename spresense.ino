a3bb14869e4b9106947a94765185e33744d453f0
dst <dst02929@gmail.com>
2024-07-25 09:59:06 +0800

    connect with rpi

--- spresense.ino
+++ spresense.ino
@@ -4,42 +4,87 @@
 #include <MP.h>
 
 
-#define BUTTON_PIN 23    // Define the pin D3 to recive the button output
-#define RELAY_PIN 27     // Define the pin D27 to connect the relay
-#define DELAY_TIME 3000  // Defines the duration of the unlock in milliseconds
+#define BUTTON_PIN 23  // Define the pin D3 to recive the button output
+
+#define LED_TRUE_PIN 25   // Define the pin D25 to indicate valid state
+#define LED_FALSE_PIN 26  //Define the pin D26 to indicate invalid state
+
+#define RPI_START_PIN 2  // Define the pin D02 to send start signal to Raspberry Pi
+#define RPI_TRUE_PIN 3   // Define the pin D03 to receive true signal from Raspberry Pi
+#define RPI_FALSE_PIN 4  // Define the pin D04 to receive false signal from Raspberry Pi
+
+#define TIMEOUT 30000  // 30 seconds timeout
 
 AudioRecorder recorder;
 SDClass theSD;
 
-void unlock() {
-  digitalWrite(RELAY_PIN, HIGH);  // Output high level unlock
-  Serial.println("Unlocked");
-}
-
-void relock() {
-  digitalWrite(RELAY_PIN, LOW);  // Output low level lock-up
-  Serial.println("Relocked");
-}
-
-
 void setup() {
   Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(RELAY_PIN, OUTPUT);
+
+  pinMode(LED_TRUE_PIN, OUTPUT);
+  digitalWrite(LED_TRUE_PIN, LOW);
+  pinMode(LED_FALSE_PIN, OUTPUT);
+  digitalWrite(LED_FALSE_PIN, LOW);
+
+
+  pinMode(RPI_TRUE_PIN, INPUT_PULLUP);
+  pinMode(RPI_FALSE_PIN, INPUT_PULLUP);
+
+  while (!theSD.begin()) {
+    Serial.println("SD card initialization failed. Retrying...");
+    delay(1000);  // wait until SD card is mounted.
+  }
+  Serial.println("SD card initialized successfully.");
+
+  Serial.println("Setup complete.");
 }
 
 void loop() {
-  while(buttonState == LOW){
-    Serial.println("Press the button to open the program");
-    delay(1000);
+
+  Serial.println("Waiting for button press.");
+  while (digitalRead(BUTTON_PIN) != HIGH) {
+    delay(100);  // Debouncing delay
   }
 
-  int buttonState = digitalRead(BUTTON_PIN);
-  if (buttonState == HIGH) {
-    Serial.println("Audio recording start!");
-    recorder.wav_recorder();
-    
+  recorder.wav_recorder();
 
+  // Start USB Mass Storage
+  theSD.beginUsbMsc();
+  Serial.println("UsbMsc started successfully.");
+  delay(3000);  //Waiting for execution
+
+  Serial.println("Waiting for response from Raspberry Pi...");
+  unsigned long startTime = millis();
+  while (true) {
+    int validState = digitalRead(RPI_TRUE_PIN);
+    int invalidState = digitalRead(RPI_FALSE_PIN);
+
+    if (validState == HIGH) {
+      Serial.println("Valid user.");
+      digitalWrite(LED_TRUE_PIN, HIGH);
+      delay(5000);
+      digitalWrite(LED_TRUE_PIN, LOW);
+      break;
+    } else if (invalidState == HIGH) {
+      Serial.println("Invalid user.");
+      for (int i = 0; i < 6; i++) {
+        digitalWrite(LED_FALSE_PIN, HIGH);
+        delay(400);
+        digitalWrite(LED_FALSE_PIN, LOW);
+        delay(400);
+      }
+      break;
+    }
+
+    if (millis() - startTime >= TIMEOUT) {
+      Serial.println("Timeout: No response from Raspberry Pi.");
+      break;
+    }
+
+    delay(100);
   }
-  
+
+  theSD.endUsbMsc();
+  Serial.println("UsbMsc stopped successfully.");
 }
